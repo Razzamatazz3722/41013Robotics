3d382c098e996147953926e1c1b483ca826f665f
Rachael Whitford <stargate@mynetspace.info>
2021-04-26 16:24:34 +1000

    added ROS subscriber for deptha nd RGB messages for the camera

--- main.m
+++ main.m
@@ -2,6 +2,24 @@ clf;
 clear;
 clc; 
 
+
+
+
 %%
 c1 = Cup(transl(1.5,1.5,0), 'RedCup.ply', 'redcup');
 
+
+
+
+%% Start ROS
+
+clearvars
+rosshutdown
+rosinit
+
+% Subscribe to depth adn RGB messages
+depthRawSub = rossubscriber('/camera/depth/image_raw'); % Depends upon the camera in use (refer to instructions)
+rgbRawSub = rossubscriber('/camera/rgb/image_raw'); % Depends upon the camera in use (refer to instructions)
+pause(2);
+depthImg = readImage(depthRawSub.LatestMessage);
+rgbImg = readImage(rgbRawSub.LatestMessage);

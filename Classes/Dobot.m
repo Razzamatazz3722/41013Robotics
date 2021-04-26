cf682dc0e7a8fe6a4871950d765d6b3ebb990404
Rachael Whitford <stargate@mynetspace.info>
2021-04-26 15:46:08 +1000

    added documentation folder and added links to Dobot folder (not correct)

--- Classes/Dobot.m
+++ Classes/Dobot.m
@@ -7,10 +7,20 @@ classdef Dobot
     end
     
     methods
-        function obj = untitled4(inputArg1,inputArg2)
-            %UNTITLED4 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function obj = Dobot()
+            
+            L1 = Link('a',0,'d',0.1519,'alpha',pi/2,'qlim',[-2*pi,2*pi])
+            L2 = Link('a',-0.24365,'d',0,'alpha',0,'qlim',[-2*pi,2*pi])
+            L3 = Link('a',-0.21325,'d',0,'alpha',0,'qlim',[-2*pi,2*pi])
+            L4 = Link('a',0,'d',0.11235,'alpha',pi/2,'qlim',[-2*pi,2*pi])
+            L5 = Link('a',0,'d',0.08535,'alpha',-pi/2,'qlim',[-2*pi,2*pi])
+            L6 = Link('a',0,'d',0.0819,'alpha',0,'qlim',[])
+            
+            obj.model = SerialLink([L1 L2 L3 L4 L5 L6],'name','UR3'); 
+            
+            q = zeros(1,6)
+            obj.model.plot(q,'fps',25,'workspace',obj.workspace,'scale',0.5);
+            
         end
         
         function outputArg = method1(obj,inputArg)

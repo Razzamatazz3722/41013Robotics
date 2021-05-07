9c634f609e1e8d4007dd46c89c71a212abd64f5c
Hayden <12555102@student.uts.edu.au>
2021-05-07 16:17:34 +1000

    Made GUI and updated Dobot class
    Dobot class now works. GUI does forward Kinematics and has a working Estop

--- Classes/Dobot.m
+++ Classes/Dobot.m
@@ -1,32 +1,42 @@
-classdef Dobot
+classdef Dobot < handle
     %UNTITLED4 Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        Property1
+         model;
+        
+        %>
+        workspace = [-0.2 1 -1 1 -0.3 1];
     end
     
     methods
-        function obj = Dobot()
-            
-            L1 = Link('a',0,'d',0.08,'alpha',90,'qlim',[deg2rad(-90),deg2rad(90)])
-            L2 = Link('a',deg2rad(130),'d',0,'alpha',0,'qlim',[0,deg2rad(85)])
-            L3 = Link('a',deg2rad(160),'d',0,'alpha',0,'qlim',[deg2rad(-10),deg2rad(90)])
-            L4 = Link('a',0,'d',0,'alpha',0,'qlim',[deg2rad(-90),deg2rad(90))
-  
-            
-            obj.model = SerialLink([L1 L2 L3 L4 L5 L6],'name','UR3'); 
-            
-            q = zeros(1,6)
-            obj.model.plot(q,'fps',25,'workspace',obj.workspace,'scale',0.5);
-            
-        end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+function self = Dobot()
+    
+    self.GetDobotRobot;
+
+    q = zeros(1,4);
+    %self.model.plot(q,'fps',25,'workspace',self.workspace,'scale',0.5);
+
+end
+      
+function GetDobotRobot(self)
+    L1 = Link('a',0,'d',0.08,'alpha',deg2rad(90),'offset',0,'qlim',[deg2rad(-90),deg2rad(90)]);
+    L2 = Link('a',0.135,'d',0,'alpha',0,'qlim',[0,deg2rad(85)]);
+    L3 = Link('a',0.16,'d',0,'alpha',0,'qlim',[deg2rad(-90),deg2rad(10)]);
+    L4 = Link('a',0,'d',0,'alpha',deg2rad(90),'qlim',[deg2rad(-90),deg2rad(90)]);
+
+    self.model = SerialLink([L1 L2 L3 L4],'name','Dobot');
+end
+    
+    
+    
+    
+    % function outputArg = method1(obj,inputArg)
+%     %METHOD1 Summary of this method goes here
+%     %   Detailed explanation goes here
+%     outputArg = obj.Property1 + inputArg;
+% end
     end
 end
 

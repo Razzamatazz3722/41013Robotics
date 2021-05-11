0a040963094745f40d325e6a632885e020106fec
Rachael Whitford <stargate@mynetspace.info>
2021-05-11 11:37:13 +1000

    Added tool functions and safety functions

--- Classes/OtherDobotFunctions.m
+++ Classes/OtherDobotFunctions.m
@@ -1,24 +1,46 @@
 classdef OtherDobotFunctions
-    %UNTITLED4 Summary of this class goes here
-    %   Detailed explanation goes here
     
     properties (Access = private)
         jointStateSubscriber;
-        endEffectorPoseSubscriber
+        endEffectorPoseSubscriber;
+        toolStateSubscriber;
+        safetyStatusSubscriber;
     end
     
     methods
         function obj = OtherDobotFunctions()
             obj.jointStateSubscriber = rossubscriber('/dobot_magician/joint_states'); % Create a ROS Subscriber to the topic joint_states
             obj.endEffectorPoseSubscriber = rossubscriber('/dobot_magician/end_effector_poses'); % Create a ROS Subscriber to the topic end_effector_poses
+            obj.toolStateSubscriber = rossubscriber('/dobot_magician/tool_state');
+            obj.safetyStatusSubscriber = rossubscriber('/dobot_magician/safety_status');
             pause(2);
+        end
+        
+        function currentDobotState = getCurrentDobotSafetyStatus(obj)
+            currentSafetyStatus = obj.safetyStatusSubscriber.LatestMessage.Data;
+            
+            if(currentSafetyStatus == 0)
+                display("DOBOT STATUS: INVALID");
+            elseif(currentSafetyStatus == 1)
+                display("DOBOT STATUS: DISCONNECTED");
+            elseif(currentSafetyStatus == 2)
+                display("DOBOT STATUS: INITIALISING");
+            elseif(currentSafetyStatus == 3)
+                display("DOBOT STATUS: ESTOPPED");
+            elseif(currentSafetyStatus == 4)
+                display("DOBOT STATUS: OPERATIONAL");
+            elseif(currentSafetyStatus == 5)
+                display("DOBOT STATUS: PAUSED");
+            elseif(currentSafetyStatus == 6)
+                display("DOBOT STATUS: STOPPED");
+            end
         end
         
         function currentJointState = getCurrentJointState(obj)
             currentJointState = obj.jointStateSubscriber.LatestMessage.Position % Get the latest message
         end
         
-        function [roll,pitch,yaw] = getCurrentEndEffectorPose()
+        function [roll,pitch,yaw] = getCurrentEndEffectorPose(obj)
             currentEndEffectorPoseMsg = obj.endEffectorPoseSubscriber.LatestMessage;
             % Extract the position of the end effector from the received message
             currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X,
@@ -31,6 +53,52 @@ classdef OtherDobotFunctions
                                       currentEndEffectorPoseMsg.Pose.Orientation.Z];
             % Convert from quaternion to euler
             [roll,pitch,yaw] = quat2eul(currentEndEffectorQuat); 
+        end
+        
+        function setTargetJointState(obj, jointTarget)
+            %jointTarget = [0,0.4,0.3,0]; % Remember that the Dobot has 4 joints by default.
+            [targetJointTrajPub,targetJointTrajMsg] = rospublisher('/dobot_magician/target_joint_states');
+            trajectoryPoint = rosmessage("trajectory_msgs/JointTrajectoryPoint");
+            trajectoryPoint.Positions = jointTarget;
+            targetJointTrajMsg.Points = trajectoryPoint;
+
+            send(targetJointTrajPub,targetJointTrajMsg);
+        end
+        
+        function setTargetEndEffectorState(obj,endEffectorPosition,endEffectorRotation)
+            %endEffectorPosition = [0.2,0,0.1];
+            %endEffectorRotation = [0,0,0];
+
+            [targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+            targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+            targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+            targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+            qua = eul2quat(endEffectorRotation);
+            targetEndEffectorMsg.Orientation.W = qua(1);
+            targetEndEffectorMsg.Orientation.X = qua(2);
+            targetEndEffectorMsg.Orientation.Y = qua(3);
+            targetEndEffectorMsg.Orientation.Z = qua(4);
+
+            send(targetEndEffectorPub,targetEndEffectorMsg); 
+        end
+        
+        function currentToolState = getCurrentToolState(obj)
+            currentToolState = obj.toolStateSubscriber.LatestMessage.Data;
+            
+            if(currentToolState == 0)
+                display("TOOL STATUS: OFF");
+            elseif(currentToolState == 1)
+                display("DOBOT STATUS: ON");
+            end
+        end
+        
+        function setToolState(obj)
+            % Turn on the tool
+            [toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
+            toolStateMsg.Data = [1]; % Send 1 for on and 0 for off 
+            send(toolStatePub,toolStateMsg);
         end
     end
 end
--- main.m
+++ main.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 clf; clear; clc; 
 
 %% Setup and Load Workspace:
@@ -10,19 +9,6 @@ Cup;
 PingPongBall; 
 Dobot;
 
-%%
-%c1 = Cup(transl(1.5,1.5,0), 'RedCup.ply', 'redcup');
-
-%% Start ROS
-
-clearvars
-rosshutdown
-rosinit
-=======
-%% Workspace Initialisation
-clf;
-clear;
-clc; 
 
 %% Robot Initialisation
 clearvars
@@ -30,8 +16,15 @@ rosshutdown
 rosinit('10.42.0.1');
 
 % Check Status of the robot
+% status = dobotSafetyStatus('/dobot_magician/safety_status');
+% if(status ==1)
+%     inp = input('Robot is not operational. Do you want to continue? [y,n]: ', 's');
+%     if inp=='n'
+%         display('program terminated');
+%     end
+% end
 status = dobotSafetyStatus('/dobot_magician/safety_status');
-if(status ==1)
+if(status ~= 4)
     inp = input('Robot is not operational. Do you want to continue? [y,n]: ', 's');
     if inp=='n'
         display('program terminated');
@@ -43,5 +36,4 @@ end
 safetyStateMsg.Data = 2;
 send(safetyStatePublisher,safetyStateMsg);
 
->>>>>>> Object_Detection
 

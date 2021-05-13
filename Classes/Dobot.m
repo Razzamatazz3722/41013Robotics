6a36d356df0de11b089a10ca74248709b7ede28f
Hayden <12555102@student.uts.edu.au>
2021-05-13 22:10:58 +1000

    fixed DH parameters and implemented collision avoidance

--- Classes/Dobot.m
+++ Classes/Dobot.m
@@ -16,7 +16,7 @@ function self = Dobot()
     self.GetDobotRobot;
 
     %q = zeros(1,4);
-    q = deg2rad([0 60 -40 0]);
+    q = deg2rad([0 30 40 0]);
     %self.model.base = transl(-1,0,0.75) *trotz(-90*pi/180);
     self.model.plot(q,'fps',25,'noarrow','workspace',self.workspace,'scale',0.5);
 
@@ -24,10 +24,10 @@ end
       
 function GetDobotRobot(self)
     %DH parameters for the dobot
-    L1 = Link('a',0,'d',0.08,'alpha',deg2rad(90),'offset',0,'qlim',[deg2rad(-90),deg2rad(90)]);
-    L2 = Link('a',0.135,'d',0,'alpha',0,'qlim',[0,deg2rad(85)]);
-    L3 = Link('a',0.16,'d',0,'alpha',0,'qlim',[deg2rad(-90),deg2rad(10)]);
-    L4 = Link('a',0,'d',0,'alpha',deg2rad(90),'qlim',[deg2rad(-90),deg2rad(90)]);
+    L1 = Link('a',0,'d',0.08,'alpha',deg2rad(90),'offset',deg2rad(180),'qlim',[deg2rad(-90),deg2rad(90)]);
+    L2 = Link('a',0.135,'d',0,'alpha',0,'offset',deg2rad(90),'qlim',[0,deg2rad(85)]);
+    L3 = Link('a',0.16,'d',0,'alpha',0,'qlim',[deg2rad(-10),deg2rad(90)]);
+    L4 = Link('a',0,'d',0,'alpha',deg2rad(90),'offset',deg2rad(180),'qlim',[deg2rad(-90),deg2rad(90)]);
 
     self.model = SerialLink([L1 L2 L3 L4],'name','Dobot');
 end
